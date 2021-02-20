cde3f3cf64e471ce1bf2f79edaa2fb62725e70f0
vickykp <vivekprajapativ8@gmail.com>
2021-02-20 23:55:37 +0530

    push notification service

---
+++ notification-service1/src/main/java/com/craterzone/notification/controller/PushNotificationController.java
@@ -0,0 +1,52 @@
+package com.craterzone.notification.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.craterzone.notification.model.PushNotificationRequest;
+import com.craterzone.notification.model.PushNotificationResponse;
+import com.craterzone.notification.service.PushNotificationService;
+
+@RestController
+@RequestMapping("api/v1/notification")
+public class PushNotificationController {
+
+	@Autowired
+    private PushNotificationService pushNotificationService;
+
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @PostMapping("/topic")
+    public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/data")
+    public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity sendSampleNotification() {
+        pushNotificationService.sendSamplePushNotification();
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+}
+
